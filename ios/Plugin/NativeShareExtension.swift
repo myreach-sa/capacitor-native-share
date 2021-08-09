94675961eae744b9c1c5668c5f834b2ee25e8a98
Raúl Ibáñez <raulibpe@gmail.com>
2021-08-09 19:15:55 +0200

    add ios example

---
+++ ios/Plugin/NativeShareExtension.swift
@@ -0,0 +1,9 @@
+//
+//  NativeShareExtension.swift
+//  Plugin
+//
+//  Created by Raúl Ibáñez Pérez on 9/8/21.
+//  Copyright © 2021 Max Lynch. All rights reserved.
+//
+
+import Foundation
---
+++ ios/Plugin/NativeShareStore.swift
@@ -0,0 +1,9 @@
+//
+//  NativeShareStore.swift
+//  Plugin
+//
+//  Created by Raúl Ibáñez Pérez on 9/8/21.
+//  Copyright © 2021 Max Lynch. All rights reserved.
+//
+
+import Foundation
